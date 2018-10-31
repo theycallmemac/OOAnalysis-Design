8cf555dda41084a25206ddd4293a97f314ef1803
jakekgrogan <jake.kgrogan@gmail.com>
2018-10-31 15:14:36 +0000

    Added imports and bug fix

--- src/server.py
+++ src/server.py
@@ -1,28 +1,25 @@
-# Import flask and socket.io
+# Import flask and socket libraries
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-# Import classes
-# from classes.square import Square
-# from classes.letter import Letter
-# from classes.word import Word
-# from classes.player import Player
-# from classes.board import board
-# from classes.rack import Rack 
-# from classes.bag import Bag
-
+# Import Game Classes
+from classes/board import Board
+from classes/letter import Letter
+from classes/square import Square
+from classes/rack import Rack 
+from classes/player import Player 
+from classes/word import Word 
+from classes/bag import Bag
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
-@app.route("/")
-def hello():
-    return "Hello"
+socketio = SocketIO(app)
 
 @socketio.on('my event')
 def handle_message(message):
     print(message)
 
 if __name__ == '__main__':
+    # Encapsulates the start up of the web server
     socketio.run(app)
