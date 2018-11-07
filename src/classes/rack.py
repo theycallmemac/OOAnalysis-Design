f97036ac3bd0a4b57e25547c5fd08d570da31f96
James McDermott <james.mcdermott7@mail.dcu.ie>
2018-11-07 14:17:37 +0000

    add square.py & rack.py

--- src/classes/rack.py
+++ src/classes/rack.py
@@ -1,34 +1,102 @@
 class Rack:
-    def __init__():
-        self.id = 1                 # placeholder
-        self.letters = []           # type Letter[]
-        self.length = len(self.letters)
+    def __init__(self, id, letters):
+        """
+        Initialises Rack object
 
+        @param id: id of Rack in the game
+        @params letters: array of letters currently in the Racl
+        @return: void
+        """
+        pass
+    
     def getId():
-        return self.id
+        """
+        Returns Id of Rack 
+
+        @param: void
+        @return: int
+        """    
+        pass
 
     def getLetters():
-        return self.letters
+        """
+        Returns Letters currently in the Rack
 
-    def setId(id):
-        self.id = id                # placeholder func
+        @param: void
+        @return: Letters[]
+        """    
+        pass
 
-    def setLetters():
-        pass                        # get letters from bag
+    def _setId(id):
+        """
+        Sets Id of Rack 
+
+        @param id: id of Rack in the game
+        @return: void
+        """    
+        pass
+
+    def _setLetters(letters):
+        """
+        Sets Letters currently in the  Rack 
+
+        @param letters: array of letters in the rack
+        @return: void
+        """    
+        pass
+
+    def add(letter):
+        """
+        Adds a new letter from the bag to the rack
+
+        @param letter: new letter that is added to the rack
+        @return: void
+        """
+        pass
 
     def canTake():
-        pass                        # check if can take from bag
+        """
+        Checks if the Bag has enough tiles to be taken from
 
-    def take():                     # add func
-        pass                        # take letter from bag
+        @param: void
+        @return: bool
+        """    
+        pass
 
-    def remove(n):
-        pass                        # removes n letters from self
+    def take():
+        """
+        Takes new letter tiles from the Bag and places them into the Rack
+
+        @param: void
+        @return: void
+        """    
+        pass
+
+    def remove(letter):
+        """
+        Removes letter(s) from the rack 
+
+        @param letter: Letter(s) to be removed from the rack
+        @return: void
+        """    
+        pass
 
     def isFull():
-        return self.length == 7
+        """
+        Checks to see if a Rack is currently full 
 
-    def swap(a):
-        for n in a:
-            self.remove(n)
-            self.take()
+        @param: void
+        @return: bool
+        """    
+        pass
+        
+    def swap(letters, bagId):
+        """
+        Swaps Letters from the Rack for Letters in the Bag
+
+        @param letters: Letter(s) to be removed from the rack
+        @param bagId: Id of bag to take letters from
+        @return: void
+        """    
+        pass
+
