230011ee21c945f5201f21a3ae52f373bbdd0557
Martynas Urbanavicius <martynas@outlook.com>
2018-11-07 14:22:27 +0000

    Add letter docstrings

--- src/classes/letter.py
+++ src/classes/letter.py
@@ -1,11 +1,77 @@
 class Letter:
-    def __init__():
-        self.id = True              # placeholder
-        self.char = a               # placeholder
-        self.score = 0              # placeholder
+    def __init__(self, letterId, char, score):
+        """
+        Initializes the letter
+        
+        @param letterId: letter id
+        @param char: letter character
+        @param score: letter score
+        @return: none
+        
+        """
+        
+        pass
+    
+    def _setId(self, LetterId):
+        """
+        
+        Sets id to a letter object
+        
+        @param LetterId: id to be assigned to a letter object
+        @return: void
+        """
+        
+        pass
+    
+    def _setChar(self, char):
+        """
+        
+        Sets char to a letter object
+        
+        @param char: char to be assigned to a letter object
+        @return: void
+        """
+        
+        pass
+    
+    def _setScore(self, score):
+        """
+        
+        Sets score to a letter object
+        
+        @param letterId: score to be assigned to a letter object
+        @return: void
+        """
+        
+        pass    
+    
+    def getId(self):
+        """
+        Gets the letters id
+        
+        @param: None
+        @return: letters id
+        
+        """
+        pass
 
-    def set():
-        pass                        # placeholder
+    def getChar(self):
+        """
+        Gets the letters Char
+        
+        @param: None
+        @return: letters char
+        
+        """
+        pass
 
-    def get():
-        pass                        # placeholder
+    def getScore(self):
+        """
+        Gets the letters score
+        
+        @param: None
+        @return: letters score
+        
+        """
+        pass
+
