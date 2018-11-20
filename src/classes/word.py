c5fd88c8baa9e4b0f082e2edc26bbbb479ef5f02
r-dog <jake.kgrogan@gmail.com>
2018-11-20 19:30:57 +0000

    add word class logic

--- src/classes/word.py
+++ src/classes/word.py
@@ -1,4 +1,5 @@
 class Word:
+
     def __init__(self, id, letters, score):
         """
         Initialises Word object
@@ -8,59 +9,68 @@ class Word:
         @param score: score int
         @return: void
         """
-        pass
+        self.id = id
+        self.letters = letters
+        self.score = score
     
+    def _setId(self, id):
+        """
+        @param id : int word id
+        @return: void
+        """
+        self.id = id
+
+    def getId(self):
+        """
+        @param: None
+        @return: id int
+        """
+        return self.id
+
     def _setScore(self, score):
         """
         @param score : int score to be assigned to word object
         @return: void
         """
-        pass
+        self.score = score
 
     def getScore(self):
         """
         @params: none
         @return: score int
         """
-        pass
+        return self.score
     
     def calcScore(self):
         """
         @params: none
         @return: word score int
         """
-        pass
+        score = 0
+        for letter in self.letters:
+            score += Word.LETTER_SCORES[letter]
+        self.score = score
 
     def _setLetters(self, letters):
         """
         @param letters: array of letters
         @return: void
         """
-        pass
+        for letter in letters:
+            self.letters.append(letter)
     
     def getLetters(self):
         """
         @params: none
         @return: array of letters
         """
-        pass
-    
-    def _setScore(self, score):
-        """
-        @param score: int score to be assigned to word
-        @return: void
-        """
-        pass
-    
-    def getScore(self):
-        """
-        @params: none
-        @return: score int
-        """
-        pass
+        return self.letters
 
     def isValid(self, dictionary):
         """
         @param dictionary: dictionary of words which will be used to check if the word is valid
         """
-        pass                       
+        word = "".join(self.letters)
+        if word in dictionary:
+            return True
+        return False
