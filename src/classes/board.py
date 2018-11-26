f0dce7736ea3a0bc1ac8e41027d1fa39b00e72e4
Connor Mulready <connor.mulready2@mail.dcu.ie>
2018-11-26 14:31:40 +0000

    Add placeLetters() tryMake()

--- src/classes/board.py
+++ src/classes/board.py
@@ -1,5 +1,6 @@
 from square import Square
 from word import Word
+import copy 
 
 class Board:
     def __init__(self):
@@ -18,6 +19,68 @@ class Board:
             if self.board[y][x] == "":
                 return True
         return False
+
+    def tryMake(b, coord):
+        x = coord[0]
+        y = coord[1]
+
+        words = []
+        i = x
+        while b[y][i] != "" and i >= 0:
+            i -= 1
+        i+=1
+        j=i
+        while b[y][j] != "" and j<15:
+            j+=1
+        j-=1
+        word = ""
+        while i<j:
+            word +=b[y][i]
+            i+=1
+        word += b[y][i]
+        words.append(word)
+
+        word = ""
+        i=y
+        while b[i][x] != "" and i >=0: 
+            i-=1
+        i+=1
+        j=i
+        while b[j][x] != "" and j < 15:
+            j+=1
+        j-=1
+        word=""
+        while i <j:
+            word += b[i][x]
+            i+=1
+        word +=b[i][x] 
+        words.append(word)
+        return words
+
+    def placeLetters(input):
+        """
+        Places users letters and checks if a valid word exists
+        @param tuple (letter,[x,y])
+        @return: set of valid words
+        """
+        words = set()
+        valids= []
+        boardC = copy.deepcopy(self.board) 
+
+        for t in input:
+            boardC[t[1][0]][t[1][1]] = t[0]
+        for t in input:
+            w = tryMake(boardC,t[1])
+            if w != []:
+                for wordt in w:
+                    if wordt.isValid():
+                        words.add(wordt)
+                        valids.append(t)
+        if len(words) and len(valids) == len(input):
+            for valid in valids:
+                self.update(valid)
+            return words
+
     def update(self, x1,y1,x2,y2): # Need to append string to board
         """
         Update the board after a valid move is played.
